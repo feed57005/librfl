f93c886578b64bb58144f7a231c00e453e32153f
Pavel Novy <feed57005@gmail.com>
2015-11-25 00:10:09 +0100

    bumped to version 1.0, protobuf, rfl-gen build integration

--- project_info.cmake
+++ project_info.cmake
@@ -5,8 +5,8 @@ set (PROJECT_CONTACT      "librfl@nartech.org")
 set (PROJECT_VENDOR       "nartech.org")
 set (ORG_WEBSITE          "librfl.nartech.org")
 
-set (MAJOR_VERSION 0)
-set (MINOR_VERSION 1)
+set (MAJOR_VERSION 1)
+set (MINOR_VERSION 0)
 set (PATCH_VERSION 0)
 
 set (BREAKPAD_URL "http://breakpad.nartech.org/bugreport.php")
